daa96694139ba7bd66958b5fcdbf399363e67c3b
hmatic <web.hmatic@gmail.com>
2018-12-11 03:33:07 +0100

    Added debugging messages

--- lib/secrets_manager/railtie.rb
+++ lib/secrets_manager/railtie.rb
@@ -4,12 +4,12 @@ require 'secrets_manager/client'
 module SecretsManager
   class Railtie < ::Rails::Railtie
     config.before_configuration do
+      puts 'Initializing secrets via Secrets Manager gem'
       secret_config = SecretsManager::Config.new(Rails.root.join('secrets.json'), Rails.env)
       puts 'Secrets.json file is missing.' if secret_config.nil?
       aws_client = SecretsManager::Client.new
-
+      puts secret_config.secrets
       secret_config.secrets.each do |secret|
-        puts secret.id
         aws_client.read_secret_json(secret.id).each do |key, value|
           ENV[key.to_s] = value
         end
