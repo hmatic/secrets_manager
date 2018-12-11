afeb899143998ef6ae56eae9344cc0007750cd39
hmatic <web.hmatic@gmail.com>
2018-12-11 07:32:41 +0100

    Small refactor

--- lib/secrets_manager.rb
+++ lib/secrets_manager.rb
@@ -1,7 +1,5 @@
 require "secrets_manager/railtie"
 
 module SecretsManager
-  def load
-    puts "alo bre"
-  end
+
 end
--- lib/secrets_manager/cli.rb
+++ lib/secrets_manager/cli.rb
@@ -3,7 +3,6 @@ require 'thor'
 module SecretsManager
   class CLI < Thor
     # secrets-manager pull
-
     desc "pull", "Pull secrets"
 
     method_option 'path',
@@ -18,31 +17,12 @@ module SecretsManager
     def pull
       require 'secrets_manager/cli/pull'
       SecretsManager::Cli::Pull.new(options).run
-      # require "secrets_manager/cli/pull"
-      # Pull.start
     end
 
-    # figaro heroku:set
-
-    # desc "heroku:set", "Send Figaro configuration to Heroku"
-
-    # method_option "app",
-    #   aliases: ["-a"],
-    #   desc: "Specify a Heroku app"
-    # method_option "environment",
-    #   aliases: ["-e"],
-    #   desc: "Specify an application environment"
-    # method_option "path",
-    #   aliases: ["-p"],
-    #   default: "config/application.yml",
-    #   desc: "Specify a configuration file path"
-    # method_option "remote",
-    #   aliases: ["-r"],
-    #   desc: "Specify a Heroku git remote"
-
-    # define_method "heroku:set" do
-    #   require "figaro/cli/heroku_set"
-    #   HerokuSet.run(options)
-    # end
+    # TODO: secrets-manager read
+    # TODO: secrets-manager delete
+    # TODO: secrets-manager update
+    # TODO: secrets-manager update-value
+    # TODO: secrets-manager create
   end
 end
