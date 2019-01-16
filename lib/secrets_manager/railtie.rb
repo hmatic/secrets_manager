1fc6314808d97787cb02c88168e22055592188b9
hmatic <web.hmatic@gmail.com>
2019-01-16 16:36:49 +0100

    Version 0.2.0 - added application.yml check

--- lib/secrets_manager/railtie.rb
+++ lib/secrets_manager/railtie.rb
@@ -4,19 +4,21 @@ require 'secrets_manager/client'
 module SecretsManager
   class Railtie < ::Rails::Railtie
     config.before_configuration do
-      puts 'Initializing secrets via Secrets Manager gem.'
-      
-      config_path = Rails.root.join('secrets.json')
-      secrets_config = SecretsManager::Config.new(config_path, Rails.env).init
-      puts 'Secrets.json file is missing.' if secrets_config.nil?
-      
-      aws_client = SecretsManager::Client.new
-      
-      secrets_config.secrets.each do |secret|
-        next if File.file?(Rails.root.join(secret.path))
+      if File.file?(Rails.root.join('config', 'application.yml'))
+        puts 'Initializing secrets via Secrets Manager gem.'
 
-        aws_client.get_secret_hash(secret.id).each do |key, value|
-          ENV[key.to_s] = value
+        config_path = Rails.root.join('secrets.json')
+        secrets_config = SecretsManager::Config.new(config_path, Rails.env).init
+        puts 'Secrets.json file is missing.' if secrets_config.nil?
+
+        aws_client = SecretsManager::Client.new
+
+        secrets_config.secrets.each do |secret|
+          next if File.file?(Rails.root.join(secret.path))
+
+          aws_client.get_secret_hash(secret.id).each do |key, value|
+            ENV[key.to_s] = value
+          end
         end
       end
     end
