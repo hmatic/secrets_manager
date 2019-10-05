3249d3be7bd8ec09042ec8d74f46092ab181363b
Hrvoje Matic <hrvoje.matic@productive.io>
2019-10-05 19:21:06 +0200

    Add read command

--- lib/secrets_manager/cli.rb
+++ lib/secrets_manager/cli.rb
@@ -12,18 +12,25 @@ module SecretsManager
       aliases: ['-e'],
       default: 'development',
       desc: 'Specify current environment'
-
     def pull
       require 'secrets_manager/cli/pull'
       SecretsManager::Cli::Pull.new(options).run
     end
 
-
-    desc "version", "Get current gem version"
-
-    def version
-      require 'secrets_manager/version'
-      puts SecretsManager::VERSION
+    # secrets-manager pull
+    desc "read NAME", "Read specific secret"
+    # method_option :name, :type => :string, :required => true
+    method_option 'path',
+      aliases: ['-p'],
+      default: 'secrets.json',
+      desc: 'Specify a json configuration file path'
+    method_option 'environment',
+      aliases: ['-e'],
+      default: 'development',
+      desc: 'Specify current environment'
+    def read(name)
+      require 'secrets_manager/cli/read'
+      SecretsManager::Cli::Read.new(options.merge(name: name)).run
     end
 
     # TODO: secrets-manager read
@@ -31,5 +38,12 @@ module SecretsManager
     # TODO: secrets-manager update
     # TODO: secrets-manager update-value
     # TODO: secrets-manager create
+
+    # secrets-manager version
+    desc "version", "Get current gem version"
+    def version
+      require 'secrets_manager/version'
+      puts SecretsManager::VERSION
+    end
   end
 end
