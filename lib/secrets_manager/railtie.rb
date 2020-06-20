837c3d70b01cb041a82d23d6801b565cc4500f01
Hrvoje Matic <hrvoje.matic@productive.io>
2020-06-20 14:38:13 +0200

    Merge versions 0.2.2 and 0.2.3 to 0.2.4

--- lib/secrets_manager/railtie.rb
+++ lib/secrets_manager/railtie.rb
@@ -1,9 +1,12 @@
 require 'secrets_manager/config'
 require 'secrets_manager/client'
+require 'secrets_manager/helpers'
 
 module SecretsManager
   class Railtie < ::Rails::Railtie
     config.before_configuration do
+      include SecretsManager::Helpers
+
       unless File.file?(Rails.root.join('config', 'application.yml'))
         puts 'Initializing secrets via Secrets Manager gem.'
 
@@ -16,8 +19,12 @@ module SecretsManager
         secrets_config.secrets.each do |secret|
           next if File.file?(Rails.root.join(secret.path))
 
-          aws_client.get_secret_hash(secret.id).each do |key, value|
-            ENV[key.to_s] = value
+          if secret.plaintext? && secret.to_file?
+            write_to_file(aws_client.get_secret_string(secret.id), secret.path)
+          else
+            aws_client.get_secret_hash(secret.id).each do |key, value|
+              ENV[key.to_s] = value
+            end
           end
         end
       end
